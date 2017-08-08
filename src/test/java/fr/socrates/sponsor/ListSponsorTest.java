274fc11a8cc2035089ac0d51377729e173865993
Cyril PHAM LE <cyril.phamle@lcdlv.fr>
2017-08-08 15:43:08 +0200

    adding : Test adding 2 sponsors

--- src/test/java/fr/socrates/sponsor/ListSponsorTest.java
+++ src/test/java/fr/socrates/sponsor/ListSponsorTest.java
@@ -3,6 +3,8 @@ package fr.socrates.sponsor;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.net.ssl.SNIServerName;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -39,6 +41,43 @@ public class ListSponsorTest {
         assertThat(contractRepresentative).isEqualTo(fetchedSponsors.get(0).getContractRepresentative());
         assertThat(contact).isEqualTo(fetchedSponsors.get(0).getContact());
         assertThat(amountOfSponsoring).isEqualTo(fetchedSponsors.get(0).getAmountOfSponsoring());
+    }
+
+    @Test
+    public void should_return_two_sponsors_when_adding_a_sponsor() {
+        SponsorList listOfOneSponsor = new SponsorList(new FakeSponsorConnector());
+        String name = "name";
+        String SIRET = "siret";
+        String SIREN = "siren";
+        String contractRepresentative="contractRepresentative";
+        String contact = "contact";
+        double amountOfSponsoring = 123;
+
+        String nameSponsorTwo = "Sponsor";
+        String SIRETSponsorTwo = "siret 2";
+        String SIRENSponsorTwo = "siren 2";
+        String contractRepresentativeSponsorTwo = "contractRepresentative 2";
+        String contactSponsorTwo = "contact 2";
+        double amountOfSponsoringSponsorTwo = 1234;
+
+        listOfOneSponsor.addSponsor(new Sponsor(name,SIRET,SIREN,contractRepresentative,contact,amountOfSponsoring));
+        listOfOneSponsor.addSponsor(new Sponsor(nameSponsorTwo, SIRETSponsorTwo, SIRENSponsorTwo, contractRepresentativeSponsorTwo, contactSponsorTwo, amountOfSponsoringSponsorTwo));
+        List<Sponsor> fetchedSponsors = listOfOneSponsor.getSponsorsList();
+
+        assertThat(2).isEqualTo(fetchedSponsors.size());
+        assertThat(name).isEqualTo(fetchedSponsors.get(0).getName());
+        assertThat(SIRET).isEqualTo(fetchedSponsors.get(0).getSIRET());
+        assertThat(SIREN).isEqualTo(fetchedSponsors.get(0).getSIREN());
+        assertThat(contractRepresentative).isEqualTo(fetchedSponsors.get(0).getContractRepresentative());
+        assertThat(contact).isEqualTo(fetchedSponsors.get(0).getContact());
+        assertThat(amountOfSponsoring).isEqualTo(fetchedSponsors.get(0).getAmountOfSponsoring());
+
+        assertThat(nameSponsorTwo).isEqualTo(fetchedSponsors.get(1).getName());
+        assertThat(SIRETSponsorTwo).isEqualTo(fetchedSponsors.get(1).getSIRET());
+        assertThat(SIRENSponsorTwo).isEqualTo(fetchedSponsors.get(1).getSIREN());
+        assertThat(contractRepresentativeSponsorTwo).isEqualTo(fetchedSponsors.get(1).getContractRepresentative());
+        assertThat(contactSponsorTwo).isEqualTo(fetchedSponsors.get(1).getContact());
+        assertThat(amountOfSponsoringSponsorTwo).isEqualTo(fetchedSponsors.get(1).getAmountOfSponsoring());
     }
 
     private class FakeSponsorConnector implements SponsorConnector {
