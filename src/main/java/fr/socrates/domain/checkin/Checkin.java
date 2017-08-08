f3ef929ff1d36f44ebf80bf0b5cf061a013f2644
JeanFrancois <jean-francois.vo@la-combe-du-lion-vert.fr>
2017-08-08 16:27:17 +0200

    should return an empty list when we add participant after twentyOne hour

--- src/main/java/fr/socrates/domain/checkin/Checkin.java
+++ src/main/java/fr/socrates/domain/checkin/Checkin.java
@@ -1,14 +1,27 @@
 package fr.socrates.domain.checkin;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by lenovo_14 on 08/08/2017.
  */
 public class Checkin {
 
+    private static final int COLD_FOOD_HOUR = 21;
+    List coldFood = new ArrayList();
+
     public List getColdFoodCount() {
-        return new ArrayList();
+        return coldFood;
+    }
+
+    public void addCheckinParticipant(Date entranceDate, int participantID) {
+
+        Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+        calendar.setTime(entranceDate);   // assigns calendar to given date
+        if (calendar.get(Calendar.HOUR_OF_DAY) > COLD_FOOD_HOUR) {
+            coldFood.add(participantID);
+        }
+
+
     }
 }
--- src/test/java/fr/socrates/domain/checkin/TestCheckin.java
+++ src/test/java/fr/socrates/domain/checkin/TestCheckin.java
@@ -1,19 +1,43 @@
 package fr.socrates.domain.checkin;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
 
 /**
  * Created by lenovo_14 on 08/08/2017.
  */
 public class TestCheckin {
+    Checkin checkin;
 
-    @Test
-
-    public void should_return_an_empty_list() {
-        Checkin checkin= new Checkin();
-        Assert.assertEquals(0,checkin.getColdFoodCount().size());
+    @Before
+    public void set_initialization() {
+        checkin = new Checkin();
     }
 
+    @Test
+    public void should_return_an_empty_list() {
+        assertThat(checkin.getColdFoodCount()).isEmpty();
+    }
 
+    @Test
+    public void should_return_an_empty_list_when_add_participant_after_twenty_one() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        String dateInString = "31-08-2017 10:20:56";
+        try {
+            checkin.addCheckinParticipant(sdf.parse(dateInString),1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        assertThat(checkin.getColdFoodCount()).isEmpty();
+    }
 }
