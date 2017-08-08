31cf0a1020c92de4a9fb40a7f35168322efb7306
Cyril PHAM LE <cyril.phamle@lcdlv.fr>
2017-08-08 15:35:36 +0200

    adding : Test adding 1 sponsor

--- src/test/java/fr/socrates/sponsor/ListSponsorTest.java
+++ src/test/java/fr/socrates/sponsor/ListSponsorTest.java
@@ -18,7 +18,27 @@ public class ListSponsorTest {
     public void should_return_empty_list_sponsors_zero(){
         SponsorList emptyListOfSponsors = new SponsorList(new FakeSponsorConnector());
         List<Sponsor> fetchedSponsors = emptyListOfSponsors.getSponsorsList();
-        Assert.assertEquals(0, fetchedSponsors.size());
+        assertThat(0).isEqualTo(fetchedSponsors.size());
+    }
+
+    @Test
+    public void should_return_size_1_when_adding_a_sponsor() {
+        SponsorList listOfOneSponsor = new SponsorList(new FakeSponsorConnector());
+        String name = "name";
+        String SIRET = "siret";
+        String SIREN = "siren";
+        String contractRepresentative="contractRepresentative";
+        String contact = "contact";
+        double amountOfSponsoring = 123;
+        listOfOneSponsor.addSponsor(new Sponsor(name,SIRET,SIREN,contractRepresentative,contact,amountOfSponsoring));
+        List<Sponsor> fetchedSponsors = listOfOneSponsor.getSponsorsList();
+        assertThat(1).isEqualTo(fetchedSponsors.size());
+        assertThat(name).isEqualTo(fetchedSponsors.get(0).getName());
+        assertThat(SIRET).isEqualTo(fetchedSponsors.get(0).getSIRET());
+        assertThat(SIREN).isEqualTo(fetchedSponsors.get(0).getSIREN());
+        assertThat(contractRepresentative).isEqualTo(fetchedSponsors.get(0).getContractRepresentative());
+        assertThat(contact).isEqualTo(fetchedSponsors.get(0).getContact());
+        assertThat(amountOfSponsoring).isEqualTo(fetchedSponsors.get(0).getAmountOfSponsoring());
     }
 
     private class FakeSponsorConnector implements SponsorConnector {
