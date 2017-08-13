db59918f43489524af9359f02e360d66fdf9a8ec
bassail <bastien.sail@lcdlv.fr>
2017-08-13 21:32:49 +0200

    Refactor et correction InMemorySponsorRepository

--- src/main/java/fr/socrates/infra/repositories/InMemorySponsorRepository.java
+++ src/main/java/fr/socrates/infra/repositories/InMemorySponsorRepository.java
@@ -1,25 +1,25 @@
 package fr.socrates.infra.repositories;
 
-import fr.socrates.domain.candidate.Candidate;
 import fr.socrates.domain.sponsor.Sponsor;
 import fr.socrates.domain.sponsor.SponsorRespository;
 
-import java.util.LinkedHashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by basti on 12/08/2017.
  */
-public class InMemorySponsorRepository implements SponsorRespository{
+public class InMemorySponsorRepository implements SponsorRespository {
+
+    private final List<Sponsor> list = new ArrayList<>();
 
     @Override
     public void addSponsor(Sponsor sponsor) {
-
+        list.add(sponsor);
     }
 
     @Override
     public List<Sponsor> getSponsorsList() {
-        return null;
+        return list;
     }
 }
