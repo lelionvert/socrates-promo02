3db197eda06b84811604d0258a141216b1befa57
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-08-16 11:38:21 +0200

    Adding : test candidateFormatter with one candidate

--- src/main/java/fr/socrates/domain/candidate/CandidateFormatter.java
+++ src/main/java/fr/socrates/domain/candidate/CandidateFormatter.java
@@ -2,9 +2,14 @@ package fr.socrates.domain.candidate;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CandidateFormatter {
-    public List<String> format(List emptyList) {
-        return Collections.singletonList("No email to print");
+    public List<String> format(List<Candidate> candidateList) {
+        if(candidateList.isEmpty()) { return Collections.singletonList("No email to print"); }
+        return candidateList
+                .stream()
+                .map(Candidate::toString)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/fr/socrates/domain/candidate/CandidateFormatterTest.java
+++ src/test/java/fr/socrates/domain/candidate/CandidateFormatterTest.java
@@ -13,6 +13,16 @@ public class CandidateFormatterTest {
 
     @Test
     public void should_return_no_email_to_print_when_the_list_is_empty() throws Exception {
-        assertThat(new CandidateFormatter().format(Collections.EMPTY_LIST)).containsExactly("No email to print");
+        final CandidateFormatter candidateFormatter = new CandidateFormatter();
+        assertThat(candidateFormatter.format(Collections.EMPTY_LIST)).containsExactly("No email to print");
     }
+
+    @Test
+    public void should_return_one_email_to_print_when_the_list_contains_one_candidate() throws Exception {
+        final CandidateFormatter candidateFormatter = new CandidateFormatter();
+        final String email = "test@test.com";
+        assertThat(candidateFormatter.format(Collections.singletonList(Candidate.withEmail(email))))
+                .containsExactly(email);
+    }
+
 }
