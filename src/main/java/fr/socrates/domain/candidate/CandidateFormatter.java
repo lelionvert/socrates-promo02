aaff43c2ba57f6b0321f031ec3f6923f15ee830c
yohan lamerand <yohan.lamerand@lcdlv.fr>
2017-08-17 11:52:52 +0200

    [#15] Refactor :using the same format in main class for candidate and sponsors

--- src/main/java/fr/socrates/domain/candidate/CandidateFormatter.java
+++ src/main/java/fr/socrates/domain/candidate/CandidateFormatter.java
@@ -5,11 +5,4 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CandidateFormatter {
-    public List<String> format(List<Candidate> candidateList) {
-        if(candidateList.isEmpty()) { return Collections.singletonList("No email to print"); }
-        return candidateList
-                .stream()
-                .map(Candidate::toString)
-                .collect(Collectors.toList());
-    }
 }
