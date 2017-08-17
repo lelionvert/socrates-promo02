ebafa1fe8c9ff46984379556a304e3e785fdb515
Cyril PHAM LE <cyril.phamle@lcdlv.fr>
2017-08-17 10:12:30 +0200

    [#5] Add : confirm a candidate test

--- src/main/java/fr/socrates/domain/attendee/ConfirmationService.java
+++ src/main/java/fr/socrates/domain/attendee/ConfirmationService.java
@@ -1,16 +1,31 @@
 package fr.socrates.domain.attendee;
 
 import fr.socrates.domain.candidate.Candidate;
+import fr.socrates.domain.candidate.CandidateRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ConfirmationService {
+  private CandidateRepository candidateRepository;
+  private List<Candidate> listAttendees;
+
+  public ConfirmationService(CandidateRepository candidateRepository) {
+    this.candidateRepository = candidateRepository;
+    listAttendees = new ArrayList<>();
+  }
+
   public List getListAttendee() {
-    return new ArrayList();
+    return listAttendees;
   }
 
   public boolean confirm(Candidate candidate) {
+    List<Candidate> foundCandidates = candidateRepository.findAll().stream().filter(e -> e.equals(candidate)).collect(Collectors.toList());
+    if (foundCandidates.size() > 0) {
+      listAttendees.add(foundCandidates.get(0));
+      return true;
+    }
     return false;
   }
 }
--- src/test/java/fr/socrates/domain/attendee/ConfirmationServiceTest.java
+++ src/test/java/fr/socrates/domain/attendee/ConfirmationServiceTest.java
@@ -1,20 +1,39 @@
 package fr.socrates.domain.attendee;
 
 import fr.socrates.domain.candidate.Candidate;
+import fr.socrates.infra.repositories.InMemoryCandidateRepository;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class ConfirmationServiceTest {
+
+  private InMemoryCandidateRepository inMemoryCandidateRepository;
+
+  @Before
+  public void setUp() throws Exception {
+    inMemoryCandidateRepository = new InMemoryCandidateRepository();
+    inMemoryCandidateRepository.save(Candidate.withEmail("test@test.fr"));
+  }
+
   @Test
   public void should_not_have_any_attendee_by_default() throws Exception {
-    assertThat(new ConfirmationService().getListAttendee()).isEmpty();
+    assertThat(new ConfirmationService(inMemoryCandidateRepository).getListAttendee()).isEmpty();
   }
 
   @Test
   public void should_not_confirm_candidate_who_does_not_exists() throws Exception {
-    ConfirmationService confirmationService = new ConfirmationService();
+    ConfirmationService confirmationService = new ConfirmationService(inMemoryCandidateRepository);
     assertThat(confirmationService.confirm(Candidate.withEmail("john@doe.fr"))).isFalse();
     assertThat(confirmationService.getListAttendee()).isEmpty();
+  }
+
+  @Test
+  public void should_confirm_one_existing_candidate() throws Exception {
+    ConfirmationService confirmationService = new ConfirmationService(inMemoryCandidateRepository);
+    assertThat(confirmationService.confirm(Candidate.withEmail("test@test.fr")))
+        .isTrue();
+    assertThat(confirmationService.getListAttendee()).containsExactly(Candidate.withEmail("test@test.fr"));
   }
 }
