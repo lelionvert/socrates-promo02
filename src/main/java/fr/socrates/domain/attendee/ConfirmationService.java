a15dd54527bb38fd6993039888d045e47e954c56
Cyril PHAM LE <cyril.phamle@lcdlv.fr>
2017-08-17 09:40:28 +0200

    [#5] Add : default cases for candidate confirmation

---
+++ src/main/java/fr/socrates/domain/attendee/ConfirmationService.java
@@ -0,0 +1,16 @@
+package fr.socrates.domain.attendee;
+
+import fr.socrates.domain.candidate.Candidate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfirmationService {
+  public List getListAttendee() {
+    return new ArrayList();
+  }
+
+  public boolean confirm(Candidate candidate) {
+    return false;
+  }
+}
