2cc0f8373975456b176b42ad51cad6fda9d26260
Cyril PHAM LE <cyril.phamle@lcdlv.fr>
2017-08-17 12:06:03 +0200

    [#5] Refactor : Extract interface for confirmation service

---
+++ src/main/java/fr/socrates/domain/attendee/ConfirmationServiceImpl.java
@@ -0,0 +1,27 @@
+package fr.socrates.domain.attendee;
+
+import fr.socrates.domain.candidate.Candidate;
+import fr.socrates.domain.candidate.CandidateService;
+
+import java.util.List;
+import java.util.Optional;
+
+class ConfirmationServiceImpl implements ConfirmationService {
+  private CandidateService candidateService;
+  private ConfirmationRepository confirmationRepository;
+
+  ConfirmationServiceImpl(CandidateService candidateService, ConfirmationRepository confirmationRepository) {
+    this.candidateService = candidateService;
+    this.confirmationRepository = confirmationRepository;
+  }
+
+  List<Candidate> getListAttendee() {
+    return confirmationRepository.getConfirmations();
+  }
+
+  boolean confirm(Candidate candidate) {
+    Optional<Candidate> foundCandidate = candidateService.findCandidate(candidate);
+    foundCandidate.ifPresent(c -> confirmationRepository.add(c));
+    return foundCandidate.isPresent();
+  }
+}
