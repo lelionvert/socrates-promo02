c0877eb0ac18251fa2e736c5e099c3fdd03bd07b
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-08-21 10:22:39 +0200

    [#05] Refacto : confirm candidate

--- src/main/java/fr/socrates/domain/attendee/ConfirmationService.java
+++ src/main/java/fr/socrates/domain/attendee/ConfirmationService.java
@@ -1,7 +1,13 @@
 package fr.socrates.domain.attendee;
 
+import fr.socrates.domain.candidate.Candidate;
+
+import java.util.List;
+
 /**
  * Created by lenovo_13 on 17/08/2017.
  */
 public interface ConfirmationService {
+    List<Candidate> getListAttendee();
+    boolean confirm(Candidate candidate);
 }
