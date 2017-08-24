be01ebb87d45ab206779b95b4906ba0450ee6ece
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-08-24 16:04:55 +0200

    [#11] Add : confirmationRepository addDiet for a participant

--- src/main/java/fr/socrates/domain/meal/Diets.java
+++
@@ -1,7 +0,0 @@
-package fr.socrates.domain.meal;
-
-public enum Diets {
-  NORMAL,
-  VEGAN,
-  PESCATARIAN
-}
--- src/test/java/fr/socrates/domain/meal/MealCoversFeature.java
+++ src/test/java/fr/socrates/domain/meal/MealCoversFeature.java
@@ -21,6 +21,15 @@ import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MealCoversFeature {
+    public static final String JOHN_DOE_FR = "john@doe.fr";
+    public static final String ARTHUR_LEROI_FR = "arthur@leroi.fr";
+    public static final String RAJESH_KOOTRAPOLI_COM = "rajesh@kootrapoli.com";
+    public static final String SHELDON_COOPER_COM = "sheldon@cooper.com";
+    public static final String LEONARD_HOFSTADTER_COM = "leonard@hofstadter.com";
+    public static final String HOWARD_WOLOWITZ_COM = "howard@wolowitz.com";
+    public static final String LEO_DAGAN_FR = "leo@dagan.fr";
+    public static final String KARA_DOC_FR = "kara@doc.fr";
+    public static final String PERSE_VAL_FR = "perse@val.fr";
     private Candidate johndoe;
     private Candidate arthurleroi;
     private Candidate rajeshkootrapoli;
@@ -30,45 +39,30 @@ public class MealCoversFeature {
     private Candidate leodagan;
     private Candidate karadoc;
     private Candidate perseval;
-    private String johndoeMail;
-    private String arthurleroiMail;
-    private String rajeshMail;
-    private String sheldonMail;
-    private String leonardMail;
-    private String howardMail;
-    private String leodaganMail;
-    private String karadocMail;
-    private String persevalMail;
     private ConfirmationService confirmationService;
     private MealService mealService;
 
     @Mock
-    Printer console;
+    private Printer console;
 
     @Before
     public void setUp() throws Exception {
-        johndoeMail = "john@doe.fr";
-        arthurleroiMail = "arthur@leroi.fr";
-        rajeshMail = "rajesh@kootrapoli.com";
-        sheldonMail = "sheldon@cooper.com";
-        leonardMail = "leonard@hofstadter.com";
-        howardMail = "howard@wolowitz.com";
-        leodaganMail = "leo@dagan.fr";
-        karadocMail = "kara@doc.fr";
-        persevalMail = "perse@val.fr";
-
-        johndoe = Candidate.withEmail(johndoeMail);
-        arthurleroi = Candidate.withEmail(arthurleroiMail);
-        rajeshkootrapoli = Candidate.withEmail(rajeshMail);
-        sheldoncooper = Candidate.withEmail(sheldonMail);
-        leonardhofstadter = Candidate.withEmail(leonardMail);
-        howardwolowitz = Candidate.withEmail(howardMail);
-        leodagan = Candidate.withEmail(leodaganMail);
-        karadoc = Candidate.withEmail(karadocMail);
-        perseval = Candidate.withEmail(persevalMail);
+        johndoe = Candidate.withEmail(JOHN_DOE_FR);
+        arthurleroi = Candidate.withEmail(ARTHUR_LEROI_FR);
+        rajeshkootrapoli = Candidate.withEmail(RAJESH_KOOTRAPOLI_COM);
+        sheldoncooper = Candidate.withEmail(SHELDON_COOPER_COM);
+        leonardhofstadter = Candidate.withEmail(LEONARD_HOFSTADTER_COM);
+        howardwolowitz = Candidate.withEmail(HOWARD_WOLOWITZ_COM);
+        leodagan = Candidate.withEmail(LEO_DAGAN_FR);
+        karadoc = Candidate.withEmail(KARA_DOC_FR);
+        perseval = Candidate.withEmail(PERSE_VAL_FR);
 
         InMemoryCandidateRepository candidateRepository = new InMemoryCandidateRepository();
         CandidateService candidateService = new CandidateServiceImpl(candidateRepository);
+        confirmationService = new ConfirmationServiceImpl(candidateRepository, new InMemoryConfirmationRepository());
+        CheckInService checkInService = new CheckInServiceImpl(new InMemoryCheckInRepository());
+        mealService = new MealServiceImpl(checkInService);
+
         candidateService.add(johndoe);
         candidateService.add(arthurleroi);
         candidateService.add(rajeshkootrapoli);
@@ -79,63 +73,51 @@ public class MealCoversFeature {
         candidateService.add(karadoc);
         candidateService.add(perseval);
 
-        confirmationService = new ConfirmationServiceImpl(candidateRepository, new InMemoryConfirmationRepository());
-        confirmationService.confirm(johndoeMail);
-        confirmationService.confirm(arthurleroiMail);
-        confirmationService.confirm(rajeshMail);
-        confirmationService.confirm(sheldonMail);
-        confirmationService.confirm(leonardMail);
-        confirmationService.confirm(howardMail);
-        confirmationService.confirm(leodaganMail);
-        confirmationService.confirm(karadocMail);
-        confirmationService.confirm(persevalMail);
+        confirmationService.confirm(JOHN_DOE_FR);
+        confirmationService.confirm(ARTHUR_LEROI_FR);
+        confirmationService.confirm(RAJESH_KOOTRAPOLI_COM);
+        confirmationService.confirm(SHELDON_COOPER_COM);
+        confirmationService.confirm(LEONARD_HOFSTADTER_COM);
+        confirmationService.confirm(HOWARD_WOLOWITZ_COM);
+        confirmationService.confirm(LEO_DAGAN_FR);
+        confirmationService.confirm(KARA_DOC_FR);
+        confirmationService.confirm(PERSE_VAL_FR);
 
-        CheckInService checkInService = new CheckInServiceImpl(new InMemoryCheckInRepository());
-        mealService = new MealServiceImpl(checkInService);
     }
 
     @Test
     public void should_have_number_of_covers_by_diet() throws Exception {
-        confirmationService.addDiet(johndoeMail, Diets.VEGAN);
-        confirmationService.addDiet(arthurleroiMail, Diets.VEGAN);
-        confirmationService.addDiet(rajeshMail, Diets.VEGAN);
-        confirmationService.addDiet(sheldonMail, Diets.PESCATARIAN);
-        confirmationService.addDiet(leonardMail, Diets.PESCATARIAN);
-        confirmationService.addDiet(howardMail);
-        confirmationService.addDiet(leodaganMail);
-        confirmationService.addDiet(karadocMail);
-        confirmationService.addDiet(persevalMail);
+        confirmationService.addDiet(JOHN_DOE_FR, Diet.VEGAN);
+        confirmationService.addDiet(ARTHUR_LEROI_FR, Diet.VEGAN);
+        confirmationService.addDiet(RAJESH_KOOTRAPOLI_COM, Diet.VEGAN);
+        confirmationService.addDiet(SHELDON_COOPER_COM, Diet.PESCATARIAN);
+        confirmationService.addDiet(LEONARD_HOFSTADTER_COM, Diet.PESCATARIAN);
 
         mealService.printAllMeals();
 
         verify(console).print("Vendredi 18 midi");
         verify(console).print("Vegan : 3");
-        verify(console).print("pescatarian : 2");
+        verify(console).print("Pescatarian : 2");
         verify(console).print("Normal : 4");
 
         verify(console).print("Vendredi 18 soir");
         verify(console).print("Vegan : 3");
-        verify(console).print("pescatarian : 2");
+        verify(console).print("Pescatarian : 2");
         verify(console).print("Normal : 4");
 
         verify(console).print("Samedi 19 midi");
         verify(console).print("Vegan : 3");
-        verify(console).print("pescatarian : 2");
-        verify(console).print("Normal : 4");
-
-        verify(console).print("Samedi 19 midi");
-        verify(console).print("Vegan : 3");
-        verify(console).print("pescatarian : 2");
+        verify(console).print("Pescatarian : 2");
         verify(console).print("Normal : 4");
 
         verify(console).print("Samedi 19 soir");
         verify(console).print("Vegan : 3");
-        verify(console).print("pescatarian : 2");
+        verify(console).print("Pescatarian : 2");
         verify(console).print("Normal : 4");
 
         verify(console).print("Dimanche 20 midi");
         verify(console).print("Vegan : 3");
-        verify(console).print("pescatarian : 2");
+        verify(console).print("Pescatarian : 2");
         verify(console).print("Normal : 4");
     }
 }
