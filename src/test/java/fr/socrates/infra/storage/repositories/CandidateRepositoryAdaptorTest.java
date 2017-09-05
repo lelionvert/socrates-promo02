f04f242f55bd0ea7189d32adcf25baa198b6ab58
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-05 12:33:22 +0200

    fix test fail in CandidateRepositoryAdaptor

--- src/test/java/fr/socrates/infra/storage/repositories/CandidateRepositoryAdaptorTest.java
+++ src/test/java/fr/socrates/infra/storage/repositories/CandidateRepositoryAdaptorTest.java
@@ -1,48 +1,41 @@
 package fr.socrates.infra.storage.repositories;
 
-import fr.socrates.SocratesApplication;
 import fr.socrates.domain.CandidateId;
-import fr.socrates.domain.candidate.*;
+import fr.socrates.domain.candidate.Candidate;
+import fr.socrates.domain.candidate.CandidateRepository;
 import fr.socrates.domain.candidate.exceptions.AddCandidateException;
 import fr.socrates.domain.candidate.exceptions.UnknownCandidateException;
 import fr.socrates.domain.candidate.exceptions.UpdateCandidateNoNewDefinitionException;
 import fr.socrates.infra.storage.entities.CandidateEntity;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {SocratesApplication.class})
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
+@RunWith(MockitoJUnitRunner.class)
 public class CandidateRepositoryAdaptorTest {
-    @Autowired
-    private JpaCandidateRepository jpaCandidateRepository;
+    @Mock
+    JpaCandidateRepository jpaCandidateRepository;
 
-    private CandidateRepository candidateRepository;
+    CandidateRepository candidateRepository;
+
 
     @Before
     public void setUp() throws Exception {
         candidateRepository = new CandidateRepositoryAdaptor(jpaCandidateRepository);
     }
 
-    @After
-    public void tearDown() throws Exception {
-        jpaCandidateRepository.deleteAll();
-    }
 
     @Test
     public void should_find_all_candidates_from_jpa_repository() throws Exception {
-        jpaCandidateRepository.save(CandidateEntity.fromDomain(Candidate.singleRoomWithEmail("hello@world.fr")));
-        assertThat(candidateRepository.findAll()).extracting("email").containsOnly("hello@world.fr");
+        candidateRepository.findAll();
+        verify(jpaCandidateRepository).findAll();
+
     }
 
     @Test(expected = AddCandidateException.class)
@@ -52,8 +45,11 @@ public class CandidateRepositoryAdaptorTest {
 
     @Test
     public void should_save_a_candidate_in_repository() throws Exception {
-        assertThat(candidateRepository.addCandidate(Candidate.singleRoomWithEmail("john@doe.fr"))).extracting("email").containsOnly("john@doe.fr");
-        assertThat(jpaCandidateRepository.findAll()).extracting("email").containsOnly("john@doe.fr");
+        final Candidate candidate = Candidate.singleRoomWithEmail("john@doe.fr");
+        final CandidateEntity candidateEntity = CandidateEntity.fromDomain(candidate);
+        given(jpaCandidateRepository.save(candidateEntity)).willReturn(candidateEntity);
+        candidateRepository.addCandidate(candidate);
+        verify(jpaCandidateRepository).save(candidateEntity);
     }
 
     @Test(expected = UpdateCandidateNoNewDefinitionException.class)
@@ -78,10 +74,18 @@ public class CandidateRepositoryAdaptorTest {
 
     @Test
     public void should_update_a_candidate_in_repository() throws Exception {
-        jpaCandidateRepository.save(CandidateEntity.fromDomain(Candidate.singleRoomWithEmail("john@doe.fr")));
-        Candidate updatedCandidate = Candidate.singleRoomWithEmail("johndoe@socrates.fr");
-        assertThat(candidateRepository.updateCandidate(updatedCandidate, "john@doe.fr")).extracting("email").containsOnly("johndoe@socrates.fr");
-        assertThat(jpaCandidateRepository.findAll()).extracting("email").containsOnly("johndoe@socrates.fr");
+        final String email = "john@doe.fr";
+        final Candidate candidate = Candidate.singleRoomWithEmail(email);
+
+        final CandidateEntity candidateEntity = CandidateEntity.fromDomain(candidate);
+        final Candidate updatedCandidate = Candidate.singleRoomWithEmail("johndoe@socrates.fr");
+
+        given(jpaCandidateRepository.findByEmail(email)).willReturn(candidateEntity);
+        given(jpaCandidateRepository.save(candidateEntity)).willReturn(CandidateEntity.fromDomain(updatedCandidate));
+
+        candidateRepository.updateCandidate(updatedCandidate, email);
+
+        verify(jpaCandidateRepository).save(candidateEntity);
     }
 
     @Test(expected = UnknownCandidateException.class)
@@ -91,8 +95,13 @@ public class CandidateRepositoryAdaptorTest {
 
     @Test
     public void should_return_candidate_when_a_candidate_has_been_found_with_its_email() throws Exception {
-        jpaCandidateRepository.save(CandidateEntity.fromDomain(Candidate.singleRoomWithEmail("john@doe.fr")));
-        assertThat(candidateRepository.findCandidateByEmail("john@doe.fr")).extracting("email").containsOnly("john@doe.fr");
+
+        final String email = "john@doe.fr";
+        final CandidateEntity candidate = CandidateEntity.fromDomain(Candidate.singleRoomWithEmail(email));
+        given(jpaCandidateRepository.findByEmail(email)).willReturn(candidate);
+        candidateRepository.findCandidateByEmail(email);
+        verify(jpaCandidateRepository).findByEmail(email);
+
     }
 
     @Test(expected = UnknownCandidateException.class)
@@ -102,7 +111,9 @@ public class CandidateRepositoryAdaptorTest {
 
     @Test
     public void should_return_candidate_when_a_candidate_has_been_found_with_its_candidate_id() throws Exception {
-        jpaCandidateRepository.save(CandidateEntity.fromDomain(Candidate.singleRoomWithEmail("john@doe.fr")));
-        assertThat(candidateRepository.findCandidateById(new CandidateId("john@doe.fr"))).isEqualTo(Candidate.singleRoomWithEmail("john@doe.fr"));
-    }
+        final String candidateID = "john@doe.fr";
+        final CandidateEntity candidate = CandidateEntity.fromDomain(Candidate.singleRoomWithEmail("john@doe.fr"));
+        given(jpaCandidateRepository.findByCandidateId(candidateID)).willReturn(candidate);
+        candidateRepository.findCandidateById(new CandidateId(candidateID));
+        verify(jpaCandidateRepository).findByCandidateId(candidateID); }
 }
